5f05d6e058d2f853834fa19c1cf46eeb057423f5
Matthieu Gaudin <mgaudin@1C1.42angouleme.fr>
2024-11-21 16:30:26 +0100

    v2.2

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mgaudin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 16:13:22 by mgaudin           #+#    #+#             */
-/*   Updated: 2024/11/20 16:55:46 by mgaudin          ###   ########.fr       */
+/*   Updated: 2024/11/20 22:23:37 by mgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,44 +15,48 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-static void settle_child_one(int filefd[2], char **av, char **envp)
+static void settle_child_one(int fd1, int filefd[2], char **av, char **envp)
 {
-        dup2(filefd[1], STDOUT_FILENO);
-        close(filefd[0]);
-        execve(av[1], ft_split(av[2], ' '), envp);
+    char    *bin_path;
+    char    *cmd;
+    char    **cmd_args;
+
+    cmd = ft_strtrim(av[2], " ");
+    cmd_args = ft_split(av[2], ' ');
+    bin_path = ft_strjoin("/usr/bin/", cmd);
+    free(cmd);
+    free(cmd_args);
+    dup2(fd1, STDIN_FILENO);
+    dup2(filefd[1], STDOUT_FILENO);
+    close(filefd[0]);
+    execve(bin_path, cmd_args, envp);
+    free(bin_path);
 }
 
-static void settle_child_two(int filefd[2], char **av, char **envp)
-{
-        dup2(filefd[0], STDIN_FILENO);
-        close(filefd[1]);
-        execve(av[2], ft_split(av[3], ' '), envp);
-}
+static void settle_child_two(int fd2, int filefd[2], char **av, char **envp)
+{}
 
-static int pipex(int fd1, int fd2, char **av, char **envp)
+static void pipex(int fd1, int fd2, char **av, char **envp)
 {
-    int     filefd[2];
-    int     status;
-    pid_t   child1;
-    pid_t   child2;
+    int filefd[2];
+    int status;
+    pid_t child1;
+    pid_t child2;
 
     if (pipe(filefd) == -1)
         return (-1);
     child1 = fork();
     if (child1 == -1)
-        return (-1);
+        return (perror("Fork : "));
     else if (child1 == 0)
-        settle_child_one(filefd, av, envp);
+        settle_child_one(fd1, filefd, av, envp);
     child2 = fork();
     if (child2 == -1)
-        return (-1);
+        return (perror("Fork :"));
     else if (child2 == 0)
-        settle_child_two(filefd, av, envp);
-    close(filefd[0]);
-    close(filefd[1]);
+        settle_child_two(fd2, filefd, av, envp);
     waitpid(child1, &status, 0);
     waitpid(child2, &status, 0);
-    return (0);
 }
 
 int main(int ac, char **av, char **envp)
@@ -61,18 +65,12 @@ int main(int ac, char **av, char **envp)
     int     fd2;
 
     if (ac != 5)
-    {
-        ft_putstr_fd("Error : please verify your number of args", 2);
         return (-1);
-    }
     fd1 = open(av[1], O_RDONLY);
     fd2 = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
-    // rediriger les fds vers les fds de la pipes
     if (fd1 < 0 | fd2 < 0)
         return (-1);
     if (pipex(fd1, fd2, av, envp) == -1)
         return (-1);
-    close(fd1);
-    close(fd2);
     return (0);
 }
